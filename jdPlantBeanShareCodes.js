1f8db40407f32859cdca51711c7ee7fe75144760
lykiseki <73599969+lykiseki@users.noreply.github.com>
2020-11-21 14:25:03 +0800

    更新助力码

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -10,6 +10,7 @@
 let PlantBeanShareCodes = [
   'cvdprphj7cixsoqbns6eertieu@zarbh2oaxmrcra7wpvtt5d2jraxz2rrz2natk6y@jc5p3tc7webr7cyahi7hheluai',//账号一的好友shareCode,不同好友中间用@符号隔开
   'cvdprphj7cixsoqbns6eertieu@zarbh2oaxmrcra7wpvtt5d2jraxz2rrz2natk6y@jc5p3tc7webr7cyahi7hheluai',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'w5twvmn6thlgvgffr5mmzvaojsqttperzjydn2q',//账号三的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.PLANT_BEAN_SHARECODES) {
